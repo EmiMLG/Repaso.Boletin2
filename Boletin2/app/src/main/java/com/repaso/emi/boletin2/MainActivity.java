4d51d84b4ca904396b1ba7d4a623bb431aa2a16b
Emi <emimlg1@gmail.com>
2017-02-06 23:25:46 +0100

    added intent

--- Boletin2/app/src/main/java/com/repaso/emi/boletin2/MainActivity.java
+++ Boletin2/app/src/main/java/com/repaso/emi/boletin2/MainActivity.java
@@ -1,5 +1,6 @@
 package com.repaso.emi.boletin2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -21,7 +22,9 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
             }
         });
 
