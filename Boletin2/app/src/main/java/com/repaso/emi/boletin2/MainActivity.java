c60048ffb2e93ac03880322e3c1b3c2c4da38705
Emi <emimlg1@gmail.com>
2017-02-06 23:04:34 +0100

    added button mainActivity and SecondActivity

--- Boletin2/app/src/main/java/com/repaso/emi/boletin2/MainActivity.java
+++ Boletin2/app/src/main/java/com/repaso/emi/boletin2/MainActivity.java
@@ -2,12 +2,32 @@ package com.repaso.emi.boletin2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.activity_main___btn_main_activity) Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+
+
+
+
+
     }
 }
