5d0d834ebd45a6c32156cd0065da0c07b2474a1b
Emi <emimlg1@gmail.com>
2017-02-07 23:29:34 +0100

    commit

--- Boletin2/app/src/main/java/com/repaso/emi/boletin2/MainActivity.java
+++ Boletin2/app/src/main/java/com/repaso/emi/boletin2/MainActivity.java
@@ -5,6 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -12,21 +16,39 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.activity_main___btn_main_activity) Button button;
+    @BindView(R.id.activity_second___image_view)ImageView imageView;
+    @BindView(R.id.activity_main___edit_text) EditText editText;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                startActivity(intent);
+                //Define el campo de texto
+                String nombre_usuario = editText.getText().toString();
+
+                //Define el bundle
+                Bundle parametros = new Bundle();
+                parametros.putString("nombre", nombre_usuario);
+
+                //Define la actividad
+                Intent i = new Intent(MainActivity.this, SecondActivity.class);
+
+                i.putExtras(parametros);
+
+                //Inicia la actividad
+                startActivity(i);
+
             }
         });
+
+
+
 
 
 
--- Boletin2/app/src/main/java/com/repaso/emi/boletin2/SecondActivity.java
+++ Boletin2/app/src/main/java/com/repaso/emi/boletin2/SecondActivity.java
@@ -2,12 +2,22 @@ package com.repaso.emi.boletin2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SecondActivity extends AppCompatActivity {
 
+    @BindView(R.id.activity_second___text_view)TextView textView;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Bundle parametros = this.getIntent().getExtras(); //Definimos el contenedor de parametros
+        String nombre = parametros.getString("nombre"); //Guardamos el parametro nombre en la variable nombre
+        textView.setText("¡Hola "+nombre+"!"); //Mostramos el texto
     }
 }
