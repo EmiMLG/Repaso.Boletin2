d7b850e826a9697f4e00d545f7c6a6fcb8c76ceb
Emi <emimlg1@gmail.com>
2017-02-08 10:56:20 +0100

    fixed problem 2

--- Boletin2/app/src/main/java/com/repaso/emi/boletin2/MainActivity.java
+++ Boletin2/app/src/main/java/com/repaso/emi/boletin2/MainActivity.java
@@ -1,14 +1,11 @@
 package com.repaso.emi.boletin2;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -16,7 +13,6 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.activity_main___btn_main_activity) Button button;
-    @BindView(R.id.activity_second___image_view)ImageView imageView;
     @BindView(R.id.activity_main___edit_text) EditText editText;
 
 
@@ -24,22 +20,25 @@ public class MainActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+
                 //Define el campo de texto
-                String nombre_usuario = editText.getText().toString();
+                String nombre_usuario = editText.getText().toString();//geText coge lo escrito en el editText y .toString lo convierte en una cadena de texto.
 
                 //Define el bundle
-                Bundle parametros = new Bundle();
-                parametros.putString("nombre", nombre_usuario);
+              //  Bundle parametros = new Bundle();
+               // parametros.putString("nombre", nombre_usuario);
 
                 //Define la actividad
                 Intent i = new Intent(MainActivity.this, SecondActivity.class);
+                i.putExtra("cadena", nombre_usuario);
 
-                i.putExtras(parametros);
+               // i.putExtras(parametros);
 
                 //Inicia la actividad
                 startActivity(i);
--- Boletin2/app/src/main/java/com/repaso/emi/boletin2/SecondActivity.java
+++ Boletin2/app/src/main/java/com/repaso/emi/boletin2/SecondActivity.java
@@ -1,23 +1,34 @@
 package com.repaso.emi.boletin2;
 
-import android.support.v7.app.AppCompatActivity;
+        import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+        import android.util.Log;
+        import android.widget.ImageView;
+        import android.widget.TextView;
 
 import butterknife.BindView;
-import butterknife.ButterKnife;
+        import butterknife.ButterKnife;
 
 public class SecondActivity extends AppCompatActivity {
 
     @BindView(R.id.activity_second___text_view)TextView textView;
+    @BindView(R.id.activity_second___image_view)ImageView imageView;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        ButterKnife.bind(this);
 
-        Bundle parametros = this.getIntent().getExtras(); //Definimos el contenedor de parametros
-        String nombre = parametros.getString("nombre"); //Guardamos el parametro nombre en la variable nombre
-        textView.setText("¡Hola "+nombre+"!"); //Mostramos el texto
+
+        Intent i = this.getIntent();
+        String recibir = i.getStringExtra("cadena");
+        Log.d("HOLA", "" + recibir);
+
+        //Bundle parametros = this.getIntent().getExtras(); //Definimos el contenedor de parametros
+        //String nombre = parametros.getString("nombre"); //Guardamos el parametro nombre en la variable nombre
+        textView.setText(recibir); //Mostramos el texto
     }
 }
